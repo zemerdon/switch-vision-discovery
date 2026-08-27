e0be4ff53af842bca18c6c2ea66af0dbd5c0995d
Switch Vision SV-Dev <switch-vision@users.noreply.github.com>
2026-08-27 16:10:37 +1000

    Add Hub Credits card

--- runtime_src/run.sh
+++ runtime_src/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-SWITCH_VISION_DISCOVERY_VERSION="2.3.18"
+SWITCH_VISION_DISCOVERY_VERSION="2.3.19"
 export SWITCH_VISION_DISCOVERY_VERSION
 
 # Persistent app options are owned by Home Assistant Supervisor. Never edit
