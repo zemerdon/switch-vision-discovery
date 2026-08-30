d30e4883af32e9ab0d12008f73cf0c033aaa60c9
Switch Vision SV-Dev <switch-vision@users.noreply.github.com>
2026-08-31 06:24:14 +1000

    Start Discovery 2.3.32 legacy UniFi SNMP support

--- runtime_src/opt/switch-vision/vendors/model_identity.sh
+++ runtime_src/opt/switch-vision/vendors/model_identity.sh
@@ -3,7 +3,7 @@
 # Local model identity extraction. Only inspect OIDs that describe this device;
 # never scan arbitrary walk text because LLDP/CDP neighbour identity can contain
 # exact model strings belonging to a different switch.
-CV_CAP_MODEL_PATTERN='WS-C[0-9A-Za-z][0-9A-Za-z._-]*|SG500X-24|SG350-20|S5735-L8P4X-A1|S5720-12TP-LI-AC|CRS328-24P-4S\+|XS1930-10|GS1900-24E|N2128PX-ON|PowerConnect[[:space:]]+5548P|ex3300-48p|J8693A|USWProHD24PoE|USWProXG8PoE'
+CV_CAP_MODEL_PATTERN='WS-C[0-9A-Za-z][0-9A-Za-z._-]*|SG500X-24|SG350-20|S5735-L8P4X-A1|S5720-12TP-LI-AC|CRS328-24P-4S\+|XS1930-10|GS1900-24E|N2128PX-ON|PowerConnect[[:space:]]+5548P|ex3300-48p|J8693A|USWProHD24PoE|USWProXG8PoE|UDM-Pro|US-8-60W|US-8-150W|US-16-XG|US-24-250W|US-48-G1'
 
 cv_cap_model_from_local_scope() {
   walk_file="$1"
@@ -43,6 +43,12 @@ cv_cap_extract_model_text() {
       [Jj]8693[Aa]) printf 'HP J8693A Switch 3500yl-48G' ;;
       [Uu][Ss][Ww][Pp][Rr][Oo][Hh][Dd]24[Pp][Oo][Ee]) printf 'USW Pro HD 24 PoE' ;;
       [Uu][Ss][Ww][Pp][Rr][Oo][Xx][Gg]8[Pp][Oo][Ee]) printf 'USW Pro XG 8 PoE' ;;
+      [Uu][Dd][Mm]-[Pp][Rr][Oo]) printf 'UDM Pro' ;;
+      [Uu][Ss]-8-60[Ww]) printf 'US 8 60W' ;;
+      [Uu][Ss]-8-150[Ww]) printf 'US-8-150W' ;;
+      [Uu][Ss]-16-[Xx][Gg]) printf 'US XG 16' ;;
+      [Uu][Ss]-24-250[Ww]) printf 'US-24-250W' ;;
+      [Uu][Ss]-48-[Gg]1) printf 'US 48' ;;
       *) printf '%s' "$model" ;;
     esac
     return 0
