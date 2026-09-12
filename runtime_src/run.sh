a6c35dfa0a2fd69a4ca8badbd277da9119965a46
root <root@sv-dev.zemerdon.com>
2026-09-12 23:54:44 +1000

    Align Ian UniFi visual mappings

--- runtime_src/run.sh
+++ runtime_src/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-SWITCH_VISION_DISCOVERY_VERSION="2.4.10"
+SWITCH_VISION_DISCOVERY_VERSION="2.4.11"
 export SWITCH_VISION_DISCOVERY_VERSION
 
 # Persistent app options are owned by Home Assistant Supervisor. Never edit
